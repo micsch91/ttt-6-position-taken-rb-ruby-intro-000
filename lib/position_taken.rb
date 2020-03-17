64ce5e6d7b28d1fd29b42a5b5a71cc32a361530c
Michael Schneider <mic.sch91@gmail.com>
2020-03-17 14:26:52 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 # code your #position_taken? method here!
 def position_taken?(board, index)
-  board = ["", " ", " ", " ", " ", " ", " ", "X", "O"]
+  board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   index = 0
 
 if board[index] == " " || "" || nil
