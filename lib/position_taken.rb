2807332e0157d9804752b5a82cfdd708ccceee28
Michael Schneider <mic.sch91@gmail.com>
2020-03-17 14:31:54 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,11 +3,11 @@ def position_taken?(board, index)
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   index = 0
 
-if board[index] == "X" || "O"
-  return true
-
 elsif board[index] == " " || "" || nil
     return false
+    
+if board[index] == "X" || "O"
+  return true
 
   end
 end
